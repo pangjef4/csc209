f6977ee17c7c032da439a042f858aed7181c328b
pangjef4 <35380561+pangjef4@users.noreply.github.com>
2018-01-12 14:02:06 -0500

    Add files via upload
    adding shell scripts

---
+++ lab1/compile_all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+gcc -Wall -std=gnu99 -g -o count lab1/count.c 
+gcc -Wall -std=gnu99 -g -o echo_arg lab1/echo_arg.c
+gcc -Wall -std=gnu99 -g -o echo_stdin lab1/echo_stdin.c
+gcc -Wall -std=gnu99 -g -o hello lab1/hello.c   
---
+++ lab1/my_commands.sh
@@ -0,0 +1,2 @@
+./echo_arg csc209 > echo_out.txt
+./echo_stdin < lab1/echo_stdin.c
